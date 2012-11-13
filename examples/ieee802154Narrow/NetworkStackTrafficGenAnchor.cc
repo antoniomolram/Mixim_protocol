8dff6c62c023e4e974e851102c94aaddc6529b27
Antonio Molina Ramirez <molram888@gmail.com>
2012-11-13 10:50:21 +0100

    Added datetime to eventlog

---
+++ examples/ieee802154Narrow/NetworkStackTrafficGenAnchor.cc
@@ -0,0 +1,148 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include "NetworkStackTrafficGenAnchor.h"
+
+#include <cassert>
+
+#include "Packet.h"
+#include "BaseMacLayer.h"
+#include "FindModule.h"
+#include "NetwToMacControlInfo.h"
+
+Define_Module(NetworkStackTrafficGenAnchor);
+
+void NetworkStackTrafficGenAnchor::initialize(int stage)
+{
+	BaseLayer::initialize(stage);
+
+	if(stage == 0) {
+		world        = FindModule<BaseWorldUtility*>::findGlobalModule();
+		delayTimer   = new cMessage("delay-timer", SEND_BROADCAST_TIMER);
+
+		arp          = FindModule<BaseArp*>::findSubModule(findHost());
+		myNetwAddr   = arp->myNetwAddr(this);
+		direccion = 3;
+		packetLength = par("packetLength");
+		packetTime   = par("packetTime");
+		pppt         = par("packetsPerPacketTime");
+		burstSize    = par("burstSize");
+		//destination  = LAddress::L3Type(par("destination").longValue());
+		destination = LAddress::L3Type(direccion);
+		nbPacketDropped = 0;
+	} else if (stage == 1) {
+		if(burstSize > 0) {
+			remainingBurst = burstSize;
+			scheduleAt(dblrand() * packetTime * burstSize / pppt, delayTimer);
+		}
+	} else {
+
+	}
+}
+
+NetworkStackTrafficGenAnchor::~NetworkStackTrafficGenAnchor() {
+	cancelAndDelete(delayTimer);
+}
+
+
+void NetworkStackTrafficGenAnchor::finish()
+{
+	recordScalar("dropped", nbPacketDropped);
+}
+
+void NetworkStackTrafficGenAnchor::handleSelfMsg(cMessage *msg)
+{
+
+	switch( msg->getKind() )
+	{
+//	case PHASE_1:
+	case SEND_BROADCAST_TIMER:
+		assert(msg == delayTimer);
+		sendUnicast();
+		remainingBurst--;
+		if(remainingBurst == 0) {
+			remainingBurst = burstSize;
+			scheduleAt(simTime() + (dblrand()*1.4+0.3)*packetTime * burstSize / pppt, msg);
+		} else {
+			scheduleAt(simTime() + packetTime * 2, msg);
+		}
+		break;
+//	case PHASE_2:
+
+
+	default:
+		EV << "Unkown selfmessage! -> delete, kind: "<<msg->getKind() <<endl;
+		delete msg;
+		break;
+	}
+}
+
+
+void NetworkStackTrafficGenAnchor::handleLowerMsg(cMessage *msg)
+{
+	Packet p(packetLength, 1, 0);
+	emit(BaseMacLayer::catPacketSignal, &p);
+	msg->getKind();
+    switch( msg->getKind() )
+        {
+        case UNICAST_MESSAGE:
+            EV << "Mensaje unicast recibido en el ancla de un nodo" << endl;
+            delete msg;
+            msg = 0;
+        break;
+        }
+}
+
+
+void NetworkStackTrafficGenAnchor::handleLowerControl(cMessage *msg)
+{
+	if(msg->getKind() == BaseMacLayer::PACKET_DROPPED) {
+		nbPacketDropped++;
+	}
+	delete msg;
+	msg = 0;
+}
+
+void NetworkStackTrafficGenAnchor::sendBroadcast()
+{
+	NetwPkt *pkt = new NetwPkt("BROADCAST_ANCHOR", BROADCAST_MESSAGE);
+	pkt->setBitLength(packetLength);
+
+	pkt->setSrcAddr(myNetwAddr);
+	pkt->setDestAddr(destination);
+
+	NetwToMacControlInfo::setControlInfo(pkt, LAddress::L2BROADCAST);
+
+	Packet p(packetLength, 0, 1);
+	emit(BaseMacLayer::catPacketSignal, &p);
+
+	sendDown(pkt);
+}
+void NetworkStackTrafficGenAnchor::sendUnicast()
+{
+    NetwPkt *pkt = new NetwPkt("BROADCAST_ANCHOR", UNICAST_MESSAGE);
+    pkt->setBitLength(packetLength);
+    pkt->setSrcAddr(myNetwAddr);
+    pkt->setDestAddr(destination);
+    NetwToMacControlInfo::setControlInfo(pkt, LAddress::L2BROADCAST);
+ //   EV << "Dirección de destino ancla:" <<    getParentModule()->getParentModule()->getSubmodule("anchor",-1)->findSubmodule("nic") << endl;
+    EV << "Destination:" <<    direccion  << endl;
+
+    Packet p(packetLength, 0, 1);
+    emit(BaseMacLayer::catPacketSignal, &p);
+
+    sendDown(pkt);
+}
+
---
+++ examples/ieee802154Narrow/NetworkStackTrafficGenAnchor.h
@@ -0,0 +1,120 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#ifndef NetworkStackTrafficGenAnchor_H_
+#define NetworkStackTrafficGenAnchor_H_
+
+#include <omnetpp.h>
+
+#include "NetwPkt_m.h"
+#include "SimpleAddress.h"
+#include "BaseLayer.h"
+#include "BaseArp.h"
+#include "BaseWorldUtility.h"
+#include "NetwToMacControlInfo.h"
+/**
+ * @brief A module to generate traffic for the NIC, used for testing purposes.
+ *
+ * @ingroup exampleIEEE802154Narrow
+ */
+class NetworkStackTrafficGenAnchor : public BaseLayer
+{
+private:
+	/** @brief Copy constructor is not allowed.
+	 */
+	NetworkStackTrafficGenAnchor(const NetworkStackTrafficGenAnchor&);
+	/** @brief Assignment operator is not allowed.
+	 */
+	NetworkStackTrafficGenAnchor& operator=(const NetworkStackTrafficGenAnchor&);
+
+public:
+	enum TrafficGenMessageKinds{
+		SEND_BROADCAST_TIMER = 1,
+		BROADCAST_MESSAGE, UNICAST_MESSAGE
+	};
+
+protected:
+
+	int packetLength;
+	simtime_t packetTime;
+	double pppt;
+	int burstSize;
+	int remainingBurst;
+	LAddress::L3Type destination;
+	long direccion;
+	long nbPacketDropped;
+	int destino;
+
+
+	BaseArp* arp;
+	LAddress::L3Type myNetwAddr;
+
+	cMessage *delayTimer;
+
+	BaseWorldUtility* world;
+
+public:
+	NetworkStackTrafficGenAnchor()
+		: BaseLayer()
+		, packetLength(0)
+		, packetTime()
+		, pppt(0)
+		, burstSize(0)
+		, remainingBurst(0)
+		, destination()
+		, nbPacketDropped(0)
+		, arp(NULL)
+		, myNetwAddr()
+		, delayTimer(NULL)
+		, world(NULL)
+	{}
+
+	virtual ~NetworkStackTrafficGenAnchor();
+
+	virtual void initialize(int stage);
+
+	virtual void finish();
+
+protected:
+	/** @brief Handle self messages such as timer... */
+	virtual void handleSelfMsg(cMessage *msg);
+
+	/** @brief Handle messages from lower layer */
+	virtual void handleLowerMsg(cMessage *msg);
+
+	/** @brief Handle control messages from lower layer */
+	virtual void handleLowerControl(cMessage *msg);
+
+	/** @brief Handle messages from upper layer */
+	virtual void handleUpperMsg(cMessage *msg)
+	{
+		opp_error("NetworkStackTrafficGenAnchor has no upper layers!");
+		delete msg;
+	}
+
+	/** @brief Handle control messages from upper layer */
+	virtual void handleUpperControl(cMessage *msg)
+	{
+		opp_error("NetworkStackTrafficGenAnchor has no upper layers!");
+		delete msg;
+	}
+
+	/** @brief Send a broadcast message to lower layer. */
+	virtual void sendBroadcast();
+
+	virtual void sendUnicast();
+};
+
+#endif
